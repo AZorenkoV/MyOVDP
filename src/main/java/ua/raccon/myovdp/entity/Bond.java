69bd1abbc17943eac5c39bdc2d32343c6412e462
TanukiPC <azorenko@gmail.com>
2026-06-09 11:12:57 +0300

    Створення сутностей

--- src/main/java/ua/raccon/myovdp/entity/Bond.java
+++ src/main/java/ua/raccon/myovdp/entity/Bond.java
@@ -1,6 +1,135 @@
 package ua.raccon.myovdp.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.*;
 
+@Entity
+@Table(name = "bonds")
 public class Bond {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 12)
+    private String isin;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String broker;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Double couponRate;
+
+    @Column(nullable = false)
+    private LocalDate purchaseDate;
+
+    @Column(nullable = false)
+    private LocalDate maturityDate;
+
+    @OneToMany(mappedBy = "bond", cascade = CascadeType.ALL, orphanRemoval = true )
+    private List<Payment> payments = new ArrayList<>();
+
+    public Bond() {
+    }
+
+    public Bond(String isin, String name, String broker, Integer quantity, Double price, Double couponRate) {
+        this.isin = isin;
+        this.name = name;
+        this.broker = broker;
+        this.quantity = quantity;
+        this.price = price;
+        this.couponRate = couponRate;
+        this.purchaseDate = LocalDate.now();
+        this.maturityDate = LocalDate.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsin() {
+        return isin;
+    }
+
+    public void setIsin(String isin) {
+        this.isin = isin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBroker() {
+        return broker;
+    }
+
+    public void setBroker(String broker) {
+        this.broker = broker;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getCouponRate() {
+        return couponRate;
+    }
+
+    public void setCouponRate(Double couponRate) {
+        this.couponRate = couponRate;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public LocalDate getMaturityDate() {
+        return maturityDate;
+    }
+
+    public void setMaturityDate(LocalDate maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
 }
---
+++ src/main/java/ua/raccon/myovdp/entity/Payment.java
@@ -0,0 +1,84 @@
+package ua.raccon.myovdp.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate paymentDate;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "bond_id", nullable = false)
+    private Bond bond;
+
+    public Payment() {}
+
+    public Payment( LocalDate paymentDate, Double amount, String type, String status, Bond bond) {
+        this.paymentDate = paymentDate;
+        this.amount = amount;
+        this.type = type;
+        this.status = status;
+        this.bond = bond;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Bond getBond() {
+        return bond;
+    }
+
+    public void setBond(Bond bond) {
+        this.bond = bond;
+    }
+}
